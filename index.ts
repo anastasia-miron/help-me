732aef7a862925ead1809ce4f1684a16bcb40145
Miron Anastasia <anastasia.miron@isa.utm.md>
2025-02-21 14:26:56 +0200

    fix cors

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ const app = new Hono();
 cron();
 
 app.use("*", cors({
-    origin: 'http://localhost:5173',
+    origin: '*',
     // allowHeaders: ['Content-Type', 'Authorization'],
     // exposeHeaders: ['Content-Type', 'Content-Length', 'Cache-Control', 'X-Accel-Buffering', 'Connection']
     credentials: true
